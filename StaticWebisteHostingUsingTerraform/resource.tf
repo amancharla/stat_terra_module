5932ea4540c470268096f93f267067fb89439be1
amancharla <amancharla@gmail.com>
2024-04-15 21:02:14 -0700

    Update resource.tf

--- StaticWebisteHostingUsingTerraform/resource.tf
+++ StaticWebisteHostingUsingTerraform/resource.tf
@@ -10,22 +10,6 @@ resource "aws_s3_bucket_public_access_block" "bucket1" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
-
-
-
-resource "aws_s3_bucket_website_configuration" "bucket1" {
-  bucket = aws_s3_bucket.bucket1.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-
-}
-
 resource "aws_s3_object" "index" {
   bucket = aws_s3_bucket.bucket1.id
   key    = "index.html"
@@ -40,6 +24,19 @@ resource "aws_s3_object" "error" {
   acl = "public-read"
   source = "${path.module}/error.html"
   content_type = "text/html"
+}
+
+resource "aws_s3_bucket_website_configuration" "bucket1" {
+  bucket = aws_s3_bucket.bucket1.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
 }
 
 resource "aws_s3_bucket_policy" "public_read_access" {
